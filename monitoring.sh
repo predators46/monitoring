014bd67c8f132de892cef28a83a8f260e1751e9d
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-05 13:07:33 +0800

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 function monitoring () {
+echo "I need to knwo the ip of the server you want to monitor..."
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+		read -rp "IP address: " -e -i "$IP" IP
 apt-get install -y gcc libgeoip-dev python-virtualenv python-dev geoip-database-extra uwsgi uwsgi-plugin-python geoipupdate
 cd /srv
 git clone https://github.com/furlongm/openvpn-monitor.git
@@ -10,10 +13,13 @@ pip install -r requirements.txt
 cp openvpn-monitor.conf.example openvpn-monitor.conf
 sed -i "s@host=localhost@host=$IP@g" openvpn-monitor.conf
 sed -i 's@port=5555@port=7505@g' openvpn-monitor.conf
-cd ~/openvpndeb/
+cd ~/monitoring/
 cp openvpn-monitor.ini /etc/uwsgi/apps-available/
 ln -s /etc/uwsgi/apps-available/openvpn-monitor.ini /etc/uwsgi/apps-enabled/
-cp ~/openvpndeb/ocs.conf /etc/nginx/conf.d/
-cp ~/openvpndeb/monitoring.conf /etc/nginx/conf.d/
-cp /etc/nginx/
+cp ~/monitoring/monitoring.conf /etc/nginx/conf.d/
+cp /etc/nginx/module-available/* /etc/nginx/conf.d/
+cp /etc/nginx/nginx/conf /etc/nginx/nginx.bak
+cp ~/monitoring/nginx.conf /etc/nginx/
+service uwsgi restart
+service nginx restart
 }
