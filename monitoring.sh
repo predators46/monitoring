810a043c9683811e1fd8bb1c5ee3365093ad6581
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-05 12:52:33 +0800

    Create monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function monitoring () {
+apt-get install -y gcc libgeoip-dev python-virtualenv python-dev geoip-database-extra uwsgi uwsgi-plugin-python geoipupdate
+cd /srv
+git clone https://github.com/furlongm/openvpn-monitor.git
+cd openvpn-monitor
+virtualenv .
+. bin/activate
+pip install -r requirements.txt
+cp openvpn-monitor.conf.example openvpn-monitor.conf
+sed -i "s@host=localhost@host=$IP@g" openvpn-monitor.conf
+sed -i 's@port=5555@port=7505@g' openvpn-monitor.conf
+cd ~/openvpndeb/
+cp openvpn-monitor.ini /etc/uwsgi/apps-available/
+ln -s /etc/uwsgi/apps-available/openvpn-monitor.ini /etc/uwsgi/apps-enabled/
+}
