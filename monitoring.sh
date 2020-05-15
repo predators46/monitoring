f2ebb01abd4b6fc78d41be7fdf3d9fed6d56bb9e
Predators <31931530+predators46@users.noreply.github.com>
2020-05-15 15:08:11 +0800

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,11 +3,12 @@
 echo "I need to know the ip of the server you want to monitor..."
 IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 		read -rp "IP address: " -e -i "$IP" IP
-sudo apt-get install -y gcc libgeoip-dev python-virtualenv python-dev geoip-database-extra uwsgi uwsgi-plugin-python nginx git
+sudo apt-get install -y gcc unzip libgeoip-dev python-virtualenv python-dev geoip-database-extra uwsgi uwsgi-plugin-python nginx git
 cd /srv
 git clone https://github.com/freetextmike/monitoring.git
-git clone https://github.com/furlongm/openvpn-monitor.git
-cd openvpn-monitor
+wget https://github.com/furlongm/openvpn-monitor/archive/1.0.0.zip
+unzip 1.0.0.zip
+cd openvpn-monitor-1.0.0
 virtualenv .
 . bin/activate
 pip install -r requirements.txt
